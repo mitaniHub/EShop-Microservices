b3b88a7e56f8d1d4c20a5fc581da95bfb01802d0
mitani797@gmail.com <mitani797@gmail.com>
2024-07-06 19:37:09 +0300

    Continue Product Implementation

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductByCategoryQuery(string Category) :IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryHandler(IDocumentSession session,ILogger<GetProductsHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsHandler called with {@Query}", query);
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
